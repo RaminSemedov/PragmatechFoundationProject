92c422693fe1a1867c25abcf67056fec68b85e9d
RaminSemedov <semedovramin1983@gmail.com>
2021-07-30 13:57:36 +0300

    Portfolio Frontend Slider

--- ProjectFrontEnd/Portfolio/js/script.js
+++ ProjectFrontEnd/Portfolio/js/script.js
@@ -1,9 +1,37 @@
-let header=document.querySelector('.header');
+const header=document.querySelector('.header');
+const info=document.querySelector('.info');
+const first= document.querySelector('.header-info-firs');
+const second=document.querySelector('.header-info-second');
+const circles=document.querySelectorAll('.inner-circle');
 
 
 function changeBg(){
     
     header.classList.toggle('header-second');
+   
 }
-setInterval(changeBg,4000);
 
+let flag=true;
+function changeText(){
+     if(flag){
+        first.style.display='block';
+        second.style.display='none';
+        circles[0].classList.add('active-circle');
+        circles[1].classList.remove('active-circle');
+        flag=false;
+     }
+     else{
+        first.style.display='none';
+        second.style.display='block';
+        circles[0].classList.remove('active-circle');
+        circles[1].classList.add('active-circle');
+        flag=true; 
+     }
+           
+    
+    
+    
+}
+changeText();
+setInterval(changeBg,4000);
+setInterval(changeText,4000);
