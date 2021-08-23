fe61b9086dea7a0e70ee89c11a6aa5c5484e53b6
RaminSemedov <semedovramin1983@gmail.com>
2021-08-23 19:37:07 +0300

    Hide Random Box

--- Works/Javascript/HideRandomBox/script.js
+++ Works/Javascript/HideRandomBox/script.js
@@ -12,21 +12,17 @@ for(let i=0;i<boxes.length;i++){
         boxes[numbers[random]].style.visibility='hidden' ;
         numbers.splice(random,1);
         counter--;
+        console.log(counter);
    })
-
    
 }
 
 container.addEventListener('click',(e)=>{
-    if(e.target.className=='container'){
-    for(let i=0;i<boxes.length;i++){
-        if(boxes[i].style.visibility=='hidden'){
+    if(e.target.className=='container' && counter==0){
+    for(let i=0;i<boxes.length;i++){        
             boxes[i].style.visibility='visible';
             counter=10;
-            numbers=[0,1,2,3,4,5,6,7,8,9];
-
-        }
-        ;
+            numbers=[0,1,2,3,4,5,6,7,8,9];        
     }
 }
 })
