e588dc671f489c2a122bcc131ee79ae36634a7db
RaminSemedov <semedovramin1983@gmail.com>
2021-08-24 19:23:43 +0300

    Show Box Function

---
+++ Works/Javascript/ShowBoxFunction/script.js
@@ -0,0 +1,31 @@
+const container=document.querySelector('.container');
+
+function createBoxes(boxSayi,sutunSayi,boxHundurluyu,araMefase){
+
+  let boxes=[];
+
+    for(let i=0;i<boxSayi;i++){
+        boxes[i]=document.createElement('div');
+        boxes[i].style.height=boxHundurluyu+'px';
+        boxes[i].style.width=boxHundurluyu+'px';
+        boxes[i].style.margin=araMefase+'px';
+        boxes[i].className='box';
+        container.appendChild(boxes[i]);       
+    }
+         container.style.gridTemplateColumns=getColumns(sutunSayi);
+       
+    
+    
+         function getColumns(sutunSayi){
+             let s='';
+             for(let i=0;i<sutunSayi;i++){
+              s+='1fr ';
+
+             }
+            return s;
+         }
+}
+
+
+
+createBoxes(20,2,50,5);
